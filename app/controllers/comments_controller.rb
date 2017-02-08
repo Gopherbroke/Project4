9d2d0904ebac6b2e23408fe3f0bf1e4f8d6328c2
Josh Owen <joshalldaylong@gmail.com>
2017-02-08 17:10:14 -0500

    saving things before devise/cancancan fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,29 @@
 class CommentsController < ApplicationController
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource  only: [:edit, :update, :destroy]
+
 
   def index
+    @user = current_user
     @piece = Piece.find(params[:piece_id])
     @comments = @piece.comments
   end
 
   def new
+    @user = current_user
     @piece = Piece.find(params[:piece_id])
     @comment = @piece.comments.build
   end
 
   def show
+    @user = current_user
     @piece = Piece.find(params[:piece_id])
     @comment = Comment.find(params[:id])
   end
 
 
   def create
+    @user = current_user
     @piece = Piece.find(params[:piece_id])
     @comment = @piece.comments.create(comment_params)
 
@@ -30,11 +37,13 @@ class CommentsController < ApplicationController
 
 
   def edit
+    @user = current_user
     @piece = Piece.find(params[:piece_id])
-    @comment = @piece.comments.find(params[:id])
+    @comment = @piece.comments.find(params[:comment_id])
   end
 
   def update
+    @user = User.find(params[:user_id])
     @piece = Piece.find(params[:piece_id])
     @comment = @piece.comments.find(params[:id])
 
@@ -47,6 +56,7 @@ class CommentsController < ApplicationController
 
 
   def destroy
+    @user = current_user
     @piece = Piece.find(params[:piece_id])
     @comment = @piece.comments.destroy(params[:id])
       redirect_to piece_path(@piece)
@@ -56,7 +66,7 @@ class CommentsController < ApplicationController
 private
   # Only allow a trusted parameter "white list" through.
 def comment_params
-  params.require(:comment).permit(:body, :piece_id)
+  params.require(:comment).permit(:body, :piece_id, :user_id)
 end
 
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :piece
+  belongs_to :user
 end
